21297fa8a0a056aa144b8d493e80ea1fd444ace2
Arun <arun@au-ki.com>
2024-06-17 10:50:58 +0530

    changes

--- public/embed-script.js
+++ public/embed-script.js
@@ -4,14 +4,10 @@
   rootDiv.id = 'calculator-root';
   rootDiv.style.width = '100%';
   rootDiv.style.height = '100%';
-  document.body.appendChild(rootDiv);
+  document.getElementById('calculator-embed').appendChild(rootDiv);
 
   // Load the React app script
   const script = document.createElement('script');
   script.src = "https://aruns2008.github.io/calculator/static/js/main.3b640362.js"; // Adjust the path if necessary
-  script.onload = function() {
-    // Optional: Initialize your React app if needed
-    // This step may not be necessary if your React app auto-initializes
-  };
   document.head.appendChild(script);
 })();
