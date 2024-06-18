49b3630f6b36c323d94ed4931e9f0c0247f22c17
Arun <arun@au-ki.com>
2024-06-18 12:31:21 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import CalculatorPage from "./pages/calculator/Calculator";
 import { CalculationStorageProvider } from "./context/StorageContext";
-
 import EstimatedResults from "./pages/EstimatedResults/EstimatedResults";
-import ScenarioViewer from "./pages/ScenarioViewer/ScenarioViewer"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ScenarioViewer from "./pages/ScenarioViewer/ScenarioViewer";
 
 function App() {
-  
   return (
     <Router basename="/calculator">
       <CalculationStorageProvider>
         <Routes>
           <Route path="/" element={<CalculatorPage />} />
           <Route path="/results" element={<EstimatedResults />} />
-          <Route path="/scenario-viewer/" element={<ScenarioViewer />} />
+          <Route path="/scenario-viewer" element={<ScenarioViewer />} />
         </Routes>
       </CalculationStorageProvider>
     </Router>
