9df7e7745e7869a69b3d2b98c5b8a9320c8c01a1
Arun <arun@au-ki.com>
2024-06-18 13:10:52 +0530

    Updates

--- embed-script.js
+++ embed-script.js
@@ -1,5 +1,4 @@
 (function() {
-  console.log("called");
   // Create a div element where the React app will be rendered
   const rootDiv = document.createElement('div');
   rootDiv.id = 'calculator-root';
@@ -7,8 +6,19 @@
   rootDiv.style.height = '100%';
   document.getElementById('calculator-embed').appendChild(rootDiv);
 
+  // Load the custom CSS file
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = "https://aruns2008.github.io/calculator/style.css";
+  document.head.appendChild(link);
+
+  // Load the custom JS file
+  const customScript = document.createElement('script');
+  customScript.src = "https://aruns2008.github.io/calculator/script.js";
+  document.head.appendChild(customScript);
+
   // Load the React app script
   const script = document.createElement('script');
-  script.src = "https://aruns2008.github.io/calculator/static/js/main.cd431078.js"; // Adjust the path if necessary
+  script.src = "https://aruns2008.github.io/calculator/static/js/main.cd431078.js"; // Ensure this matches your build output
   document.head.appendChild(script);
 })();
